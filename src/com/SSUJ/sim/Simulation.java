9d803f1d266d5a85bd825a50438431b75f5c2965
herasymj <jennifer.herasymuik@gmail.com>
2016-11-01 15:49:21 -0600

    Added UI package, made changes to sim.

--- src/com/SSUJ/sim/Simulation.java
+++ src/com/SSUJ/sim/Simulation.java
@@ -1,11 +1,13 @@
 package com.SSUJ.sim;
 
-/*
+import com.SSUJ.ui.UI;
+import com.SSUJ.world.World;
+/*****************************************************************************************************
  * Simulation
  * 
  * Purpose: This is the base of the simulation. It has access to a user interface, contains a length
  * 		that the sim will run for, as well as a world the sim is run in.
- */
+ *****************************************************************************************************/
 
 public class Simulation {
 
---
+++ src/com/SSUJ/ui/UI.java
@@ -0,0 +1,36 @@
+package com.SSUJ.ui;
+
+import java.util.Scanner;
+/*******************************************************************************
+ * UI
+ * 
+ * Purpose: This is the user interface.
+ * 
+ *********************************************************************************/
+
+public class UI {
+	
+	private Scanner in;
+	private System.out out;
+	
+	public Scanner getIn(){
+		
+	}
+	
+	public System.out getOut(){
+		
+	}
+	
+	public void instructions(){
+		
+	}
+	
+	public void log(string s){
+		
+	}
+	
+	public void daysRemaining(){
+		
+	}
+
+}
