0e6cb15b8a2c7a93f9b28b1546860486d52a74ab
herasymj <jennifer.herasymuik@gmail.com>
2016-11-01 15:37:13 -0600

    Started coding the menu and simulation classes. Stub coding only.

---
+++ src/com/SSUJ/menu/Menu.java
@@ -0,0 +1,27 @@
+package com.SSUJ.menu;
+
+import com.SSUJ.sim.Simulation;
+/*******************************************************************************
+ * Menu
+ * 
+ * Purpose: This provides the user the ability to start the simulation, learn
+ * 		about the simulation, or quit the simulation.
+ * 
+ *********************************************************************************/
+public class Menu {
+	
+	private Simulation simulation; //sim that will be run
+	
+	public void begin(){
+		
+	}
+	
+	public void quit(){
+		
+	}
+	
+	public void about(){
+		
+	}
+
+}
---
+++ src/com/SSUJ/sim/Simulation.java
@@ -0,0 +1,36 @@
+package com.SSUJ.sim;
+
+/*
+ * Simulation
+ * 
+ * Purpose: This is the base of the simulation. It has access to a user interface, contains a length
+ * 		that the sim will run for, as well as a world the sim is run in.
+ */
+
+public class Simulation {
+
+	private World world;//world sim will be ran in
+	private int length;
+	private UI ui;
+	
+	public World getWorld(){
+		
+	}
+	
+	public int getLength(){
+		
+	}
+	
+	public int setLength(){
+		
+	}
+	
+	public void initialize(){
+		
+	}
+	
+	public UI getUI(){
+		
+	}
+	
+}
