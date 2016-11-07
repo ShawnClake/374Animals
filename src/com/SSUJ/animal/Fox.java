f8c6624134457634754b16fea47ee2ef65e552d7
Uyser <uyskriek@gmail.com>
2016-11-07 16:31:37 -0600

    Add files via upload

--- src/com/SSUJ/animal/Fox.java
+++ src/com/SSUJ/animal/Fox.java
@@ -2,4 +2,25 @@ package com.SSUJ.animal;
 
 public class Fox extends Animal {
 
+	
+public void generate(){
+		
+	Random ran = new Random();
+	int x = ran.nextInt(11) + 30;		//Random number between 30-40
+	
+	
+		eatsLevel = 5;
+		hunger = 0;		
+		health = x;
+		speed = 4;
+		maxHealth = 70;
+		maxHunger = 70;
+		
+		name = "Fox";
+		description = "Fox: Eats bluejays, squirrels, mice, and rabbits. Is an apex predator.";
+				
+		
+		
+		
+	}
 }
