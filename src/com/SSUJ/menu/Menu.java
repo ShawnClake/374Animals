afef4c24e162011aebfd94e63cfa590e66aee790
herasymj <jennifer.herasymuik@gmail.com>
2016-11-12 23:00:24 -0600

    Finished the main function in the menu class

--- src/com/SSUJ/menu/Menu.java
+++ src/com/SSUJ/menu/Menu.java
@@ -16,23 +16,48 @@ public class Menu {
 	public static void main(String [] args)
 	{
 		System.out.println("Welcome to Animals!");
-		System.out.println("Please select one of the following options:");
-		System.out.println("Begin -> begin the simulation.");
-		System.out.println("Quit -> quit the simulation.");
-		System.out.println("About -> get information about this simulation.");
-		
+		String choice = "";
+		RUN:
+		while(!choice.equals("quit")){
+			System.out.println("Please select one of the following options:");
+			System.out.println("Begin -> begin the simulation.");
+			System.out.println("Quit -> quit the simulation.");
+			System.out.println("About -> get information about this simulation.");
+			
+			Scanner in = new Scanner(System.in);
+			
+			System.out.println("Enter either \"Begin\", \"Quit\", or \"About\":");
+			choice = in.nextLine();
+			
+			choice = choice.toLowerCase();
+			while(choice != "begin" || choice != "quit" || choice != "about"){
+				System.out.println("That was not a correct choice.");
+				System.out.println("Please enter either 'begin', 'quit', or 'about'");
+			}
+			if(choice.equals("begin")){
+				begin();
+				continue RUN;
+			}
+			if(choice.equals("about")){
+				about();
+				continue RUN;
+			}
+			in.close();
+		}
+		//quitting the program
+		quit();
 		
 	}
 	
-	public void begin(){
+	public static void begin(){
 		
 	}
 	
-	public void quit(){
+	public static void quit(){
 		System.out.println("Quitting the simulation...");
 	}
 	
-	public void about(){
+	public static void about(){
 		
 	}
 
